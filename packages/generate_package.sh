80bf4ccce7a40632e54ab8d8599988fd8d40d943
Ariel Martin <ariel.martin@wazuh.com>
2024-06-06 15:02:02 -0300

    Fix symbol upload

--- packages/generate_package.sh
+++ packages/generate_package.sh
@@ -99,12 +99,11 @@ build_pkg() {
         ${REVISION} ${JOBS} ${DEBUG} \
         ${CHECKSUM} ${FUTURE} ${LEGACY} ${SRC}|| return 1
 
-    #echo "Packages $(ls -Art ${OUTDIR} | tail -n 2) added to ${OUTDIR}."
     echo "**********************************************************"
-    echo "Packages generated:"
-    find ${OUTDIR} -type f -name "*(-dbg|-debuginfo|.deb|.rpm|.zip)"
-    echo "**********************************************************"
-    
+    echo "Listing of output directory: ${OUTDIR}."
+    ls -Art ${OUTDIR}
+    echo "**************** End generate_package.sh ******************"
+
     return 0
 }
 
